68511bcc427aaab62e6755e4d9ab1924be1419e5
Cpatrici23 <claudia.munozgarcia@gmail.com>
2023-05-29 22:46:24 +0200

    Componente Playa

--- voyage-vista/src/app/travels/playa/playa.component.ts
+++ voyage-vista/src/app/travels/playa/playa.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IVivienda } from '../models/vivienda.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-playa',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./playa.component.css']
 })
 export class PlayaComponent {
+  constructor(private router: Router){}
+  viviendas: IVivienda[] = []
+
+
+  ngOnInit() {
+    this.viviendas = [
+      {isAvailable: true, id:1 ,img:'../../../assets/img/1imgplaya.webp',title:'azulprofundo'},
+      {isAvailable:false, id:2 ,img: '../../../assets/img/2imgplaya.webp',title:'azulinfinito'},
+      {isAvailable:true, id:3 ,img: '../../../assets/img/3imgplaya.webp',title:'azulclarito'},
+      {isAvailable:false, id:4 ,img: '../../../assets/img/4imgplaya.webp',title:'azulaguamarina'},
+      {isAvailable:false, id:5 ,img: '../../../assets/img/5imgplaya.webp',title:'azulmediterraneo'},
+      {isAvailable:false, id:6 ,img: '../../../assets/img/6imgplaya.webp',title:'azulcielo'},
+
+
+    ]
+  }
+
+  navigateToOtherComponent() {
+    this.router.navigateByUrl('/playa');
+  }
+
 
 }
