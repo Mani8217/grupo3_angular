47d0cdafb00d8060bdfa623795a150b09c38ea76
Cpatrici23 <claudia.munozgarcia@gmail.com>
2023-05-30 00:26:02 +0200

    viajero.models.ts-modificado

--- voyage-vista/src/app/travels/models/viajero.model.ts
+++ voyage-vista/src/app/travels/models/viajero.model.ts
@@ -1,2 +1,12 @@
 export interface IViajero {
+  id: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  adress:{
+    street: String; 
+    city: string;
+    zicode: string;
+
+  }
 }
